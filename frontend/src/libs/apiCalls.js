822eb5417a0192e180ed26dc38186017138b36c2
suryakrishnakishore <suryakrishnakishore@gmail.com>
2025-06-15 13:56:31 +0530

    Modified fronted for deploy

--- frontend/src/libs/apiCalls.js
+++ frontend/src/libs/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api-v1";
+const API_URL = "https://movies-web-app-backed.onrender.com/api-v1";
 
 const api = axios.create({
     baseURL: API_URL,
