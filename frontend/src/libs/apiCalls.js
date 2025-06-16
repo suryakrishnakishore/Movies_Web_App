99927a7f3a555bd25a5c0586dd509cdc0da652d7
suryakrishnakishore <suryakrishnakishore@gmail.com>
2025-06-16 09:23:57 +0530

    Modified urls for deploy

--- frontend/src/libs/apiCalls.js
+++ frontend/src/libs/apiCalls.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.BACKEND_API_URL}/api-v1`;
+console.log("Backend url: ", import.meta.env.VITE_BACKEND_API_URL);
+
+const API_URL = `${import.meta.env.VITE_BACKEND_API_URL}/api-v1`;
 
 const api = axios.create({
     baseURL: API_URL,
